d0f3a5714712767ddb82fb31c3bd16796b59aeeb
KeenGWatanabe <mckeenweb@gmail.com>
2025-02-21 22:56:57 +0800

    output file

--- output.tf
+++ output.tf
@@ -1,3 +1,10 @@
 output "roger_igw" {
  value = aws_internet_gateway.roger_igw
 }
+output "vpc_id" {
+  value = aws_vpc.this.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public.*.id
+}
