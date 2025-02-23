8e3d178f2107942a78951ca0fe3cfa9b2bbefdeb
KeenGWatanabe <mckeenweb@gmail.com>
2025-02-23 09:28:53 +0800

    remove EIP, change name rger

--- variables.tf
+++ variables.tf
@@ -13,8 +13,8 @@ variable "subnet_count" {
   description = "Number of subnets"
   type = map(number)
   default = {
-    public = 1,
-    private = 0
+    public = 2,
+    private = 2
   }
 }
 
@@ -23,6 +23,13 @@ variable "public_subnet_cidr" {
   type = list(string)
   default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
 }
+variable "private_subnet_cidr" {
+  description = "Available CIDR-private subnets"
+  type = list(string)
+  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24", "10.0.104.0/24"]
+}
+
+
 variable "settings" {
   description = "Configuration settings"
   type = map(any)
